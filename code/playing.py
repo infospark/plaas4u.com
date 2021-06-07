d72935a8e9e85f7d5a00e5ca66a35e9607c9c6eb
nico_marais <nico.marais@gmail.com>
2021-06-07 15:43:48 +0100

    Farms filtered by price (R15 000 000)

--- code/playing.py
+++ code/playing.py
@@ -5,19 +5,28 @@ import csv
 def main():
     with open('../data/Plaas4UListings.csv') as file:
         original_list = csv.DictReader(file)
-        big_farm_list = filterBySize(original_list, 30)
+        #big_farm_list = filterBySize(original_list, 30)
+        big_farm_list = filterByPrice(original_list, 15000000)
         for farm in big_farm_list:
             print(farm)
 
 
-def filterBySize(farmList, minimum_size):
-    filteredList = []
+#def filterBySize(farmList, minimum_size):
+#    filteredList = []
+#    for row in farmList:
+#        size = float(row["Size (ha)"])
+#        if size >= minimum_size:
+#            filteredList.append(row)
+#    return filteredList
+
+
+def filterByPrice(farmList, maximum_price):
+    filteredList =[]
     for row in farmList:
-        size = float(row["Size (ha)"])
-        if size >= minimum_size:
+        price = float(row["Price (Rand)"])
+        if price <= maximum_price:
             filteredList.append(row)
     return filteredList
-
 
 if __name__ == "__main__":
     main()
