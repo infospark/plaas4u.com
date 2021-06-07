4ed67507e7bcbd364b482b65a6e5afe1b479beda
nico_marais <nico.marais@gmail.com>
2021-06-07 15:01:02 +0100

    Farms loaded from CSV

--- code/playing.py
+++ code/playing.py
@@ -1,5 +1,16 @@
+import csv
+
+
 def main():
-    print("Hello World!")
+    filteredList = []
+    with open('../data/Plaas4UListings.csv') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            size = float(row["Size (ha)"])
+            if size >= 30:
+                filteredList.append(row)
+    print(filteredList)
+
 
 if __name__ == "__main__":
     main()
