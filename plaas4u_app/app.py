a26892eb0e49ef258afd72efa4c80f313527273d
nico_marais <nico.marais@gmail.com>
2021-06-16 13:42:11 +0100

    Unit test for filter_by_size

--- plaas4u_app/app.py
+++ plaas4u_app/app.py
@@ -2,5 +2,19 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def root():
+    return '<h1>Hello, World!</h1>'
+
+@app.route('/Test')
+@app.route('/test')
+def test():
+    return '<h1>This is a test.</h1>'
+
+@app.route('/farms')
+def farms():
+    html = "<table>"
+    html += "<tr><th>Name</th><th>Size</th></tr>"
+    html += "<tr><td>Farm 1</td><td>100Ha</td></tr>"
+    html += "<tr><td>Farm 2</td><td>50Ha</td></tr>"
+    html += "</table>"
+    return html
